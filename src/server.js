ca0bbe07093d75109986b749d500c5e76cb0151d
NguyenNguyen <nguyenbk92@gmail.com>
2021-10-15 15:02:21 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+
+'use strict'
+
+const express = require('express');
+var cors = require('cors')
+var app = express();
+app.use(cors())
+const config = require('config')
+
+// error handler
+app.use(require('./middlewares/error'))
+// apis
+app.use(require('./apis'))
+
+var server = app.listen(config.get('server.port'), config.get('server.host'), function () {
+   console.log("TomoScan api listening at %s:port %s", config.get('server.port'), config.get('server.host'))
+})
